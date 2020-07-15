719708d4a54d39e42921bae3c314d0d21e18d99e
tangphonggmailcom <35770412+tangphonggmailcom@users.noreply.github.com>
2020-07-15 20:51:31 +0700

    Update scapy_ap_detect_log.py

--- scapy_ap_detect_log.py
+++ scapy_ap_detect_log.py
@@ -2,8 +2,8 @@
 from scapy.all import *
  
 interface = 'wlan0'
- 
 known={}
+
 def callback(frame):
   if frame.haslayer(Dot11):
     if frame.haslayer(Dot11Beacon) or frame.haslayer(Dot11ProbeResp):
@@ -15,5 +15,6 @@ def callback(frame):
         channel = int(channel.encode('hex'), 16)
         print "SSID: '{}', BSSID: {}, channel: {}".format(ssid, source, channel)
         known[source]=True
- 
+
+print "Detecting unknown access point..."
 sniff(iface=interface, prn=callback)
